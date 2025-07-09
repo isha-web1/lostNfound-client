030f39dd0940502f04e9d713907559c07091f4f6
isha-web1 <isha.web1@gmail.com>
2025-07-10 01:10:10 +0600

    create user registration & tanstack query initialization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import "@/src/styles/globals.css";
-
-import clsx from "clsx";
 import { Metadata, Viewport } from "next";
+import { Link } from "@heroui/link";
+import clsx from "clsx";
 
-import { Providers } from "./providers";
-
-import { fontSans } from "@/src/config/fonts";
 import { siteConfig } from "@/src/config/site";
+import { fontSans } from "@/src/config/fonts";
+import { Navbar } from "@/src/components/UI/navbar";
+import { Providers } from "../lib/Providers";
 
 export const metadata: Metadata = {
   title: {
@@ -36,7 +36,7 @@ export default function RootLayout({
       <head />
       <body
         className={clsx(
-          "min-h-screen text-foreground bg-background font-sans antialiased",
+          "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}
       >
---
+++ src/lib/Providers/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import * as React from "react";
+import {HeroUIProvider} from "@heroui/system";
+import { useRouter } from "next/navigation";
+import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from "next-themes";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+
+export interface ProvidersProps {
+  children: React.ReactNode;
+  themeProps?: ThemeProviderProps;
+}
+
+const queryClient = new QueryClient();
+
+export function Providers({ children, themeProps }: ProvidersProps) {
+  const router = useRouter();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HeroUIProvider navigate={router.push}>
+        <Toaster />
+        <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+      </HeroUIProvider>
+    </QueryClientProvider>
+  );
+}
