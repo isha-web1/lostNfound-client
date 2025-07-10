3b224743fd9056a27f35b4ca687d6bc47dae375a
isha-web1 <isha.web1@gmail.com>
2025-07-10 22:01:52 +0600

    create profile sidebar & create post form

--- src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
@@ -1,11 +1,66 @@
-import React from 'react';
+"use client";
 
-const page = () => {
-    return (
-        <div>
-            <h1>this is create post page</h1>
-        </div>
-    );
-};
+import FXInput from "@/src/components/form/FXInput";
+import { Divider } from "@heroui/divider";
+import { Button } from "@heroui/button";
+import {
+  FieldValues,
+  FormProvider,
+  SubmitHandler,
+  useFieldArray,
+  useForm,
+} from "react-hook-form";
 
-export default page;
+export default function page() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const methods = useForm();
+
+  const { control, handleSubmit } = methods;
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "questions",
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    const postData = {
+      ...data,
+      questions: data.questions.map((que: { value: string }) => que.value),
+    };
+
+    console.log(postData);
+  };
+
+  const handleFieldAppend = () => {
+    append({ name: "questions" });
+  };
+
+  return (
+    <div>
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FXInput name="title" label="Title" />
+
+          <Divider className="my-5" />
+
+          <div className="flex justify-between items-center">
+            <h1 className="text-xl">Owner verification questions</h1>
+            <Button onClick={() => handleFieldAppend()}>Append</Button>
+          </div>
+
+          {fields.map((field, index) => (
+            <div key={field.id} className="flex items-center">
+              <FXInput name={`questions.${index}.value`} label="Question" />
+              <Button onClick={() => remove(index)}>Remove</Button>
+            </div>
+          ))}
+
+          <Divider className="my-5" />
+
+          <Button type="submit">Post</Button>
+        </form>
+      </FormProvider>
+    </div>
+  );
+}
